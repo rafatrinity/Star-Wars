1b31986163d918339eec3df5b284e1b1ce16d963
RAFAEL TRINDADE <rafaeltrindade649@gmail.com>
2019-12-15 23:13:59 +0000

    iniciando agregação

--- Star-Wars/src/app/card/card.module.ts
+++ Star-Wars/src/app/card/card.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card.component';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 import { CrouselComponent } from '../crousel/crousel.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PopupModule } from '../popup/popup.module';
 
 @NgModule({
   declarations: [CardComponent, CrouselComponent],
-  imports: [CommonModule, MatCardModule, MatButtonModule, NgbModule],
+  imports: [CommonModule, MatCardModule, NgbModule, PopupModule],
   exports: [CardComponent, CrouselComponent]
 })
 export class CardModule { }
