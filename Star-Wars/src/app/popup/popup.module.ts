636a6546672429921f4e5d4e3f675168ec9005fd
RAFAEL TRINDADE <rafaeltrindade649@gmail.com>
2019-12-15 22:45:27 +0000

    estrutura de popup feita

---
+++ Star-Wars/src/app/popup/popup.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PopupComponent } from './popup.component';
+
+@NgModule({
+  declarations: [PopupComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PopupModule { }
