4bd740c5c69b77f6225685adf3d33592a88a7a43
Da Trindade, Rafael <rafael.da.trindade@avanade.com>
2019-12-23 15:42:00 -0300

    update angular

--- Star-Wars/src/app/popup/popup.component.ts
+++ Star-Wars/src/app/popup/popup.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
+export interface DialogData {
+  animal: 'panda' | 'unicorn' | 'lion';
+}
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
   styleUrls: ['./popup.component.scss']
 })
-export class PopupComponent implements OnInit {
 
-  constructor() { }
+export class PopupComponent {
+  constructor(public dialog: MatDialog) {}
 
-  ngOnInit() {
+  openDialog() {
+    this.dialog.open(DialogDataExampleDialog, {
+      data: {
+        animal: 'panda'
+      }
+    });
   }
+}
 
+@Component({
+  selector: 'dialog-data-example-dialog',
+  templateUrl: 'content.html',
+})
+export class DialogDataExampleDialog {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
 }
