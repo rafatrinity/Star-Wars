8c43c1b49240145acd8c4c5b902177b0a0a5f594
Da Trindade, Rafael <rafael.da.trindade@avanade.com>
2019-12-28 14:58:28 -0300

    eliminando earth module

--- Star-Wars/src/app/popup/popup.component.ts
+++ Star-Wars/src/app/popup/popup.component.ts
@@ -10,8 +10,23 @@ import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export class PopupComponent {
   constructor(public dialog: MatDialog) { }
+  palette = [
+    {
+      temperate: {
+        ocean: '#1979ff',
+        continent: '#26bd15'
+      }
+    },
+    {
+      arid: {
+        ocean: '#cc8d10',
+        continent: '#acbd15'
+      }
+    }
+  ];
 
   openDialog() {
+    // tslint:disable-next-line: no-use-before-declare
     const dialogRef = this.dialog.open(Content, {
       width: '80%',
       maxHeight: '70%'
@@ -26,4 +41,5 @@ export class PopupComponent {
 @Component({
   templateUrl: 'content.html',
 })
+// tslint:disable-next-line: component-class-suffix
 export class Content { }
--- Star-Wars/src/app/popup/popup.module.ts
+++ Star-Wars/src/app/popup/popup.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PopupComponent, Content } from './popup.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material';
-import { EarthModule } from '../earth/earth.module';
 @NgModule({
   declarations: [
     PopupComponent,
     Content
   ],
   imports: [
-    EarthModule,
     MatButtonModule,
     CommonModule,
     MatDialogModule
