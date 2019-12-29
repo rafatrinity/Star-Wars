4b0ba98df110e7070ceb582b7216fe1091624655
Da Trindade, Rafael <rafael.da.trindade@avanade.com>
2019-12-28 22:50:21 -0300

    tentando passar parametros

--- Star-Wars/src/app/earth/earth.component.ts
+++ Star-Wars/src/app/earth/earth.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-earth',
@@ -21,10 +21,17 @@ export class EarthComponent implements OnInit {
       }
     }
   ];
-
+  @Input() climate: string;
+  ocean = '#1979ff';
+  continent;
   constructor() { }
 
   ngOnInit() {
+    // this.continent = this.palette[this.climate].continent;
+    // this.ocean = this.palette[this.climate].ocean;
+    console.log(this.climate);
   }
-
+  ola(){
+    return '#1979ff';
+  }
 }
